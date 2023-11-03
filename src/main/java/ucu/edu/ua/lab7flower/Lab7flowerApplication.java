d3e1892b768ede8d09d25e0f3cb93e737c3ccd84
zahar-kohut <zaharentv2005@gmail.com>
2023-11-03 16:12:20 +0200

    created project

---
+++ src/main/java/ucu/edu/ua/lab7flower/Lab7flowerApplication.java
@@ -0,0 +1,12 @@
+package ucu.edu.ua.lab7flower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab7flowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab7flowerApplication.class, args);
+	}
+}
---
+++ src/test/java/ucu/edu/ua/lab7flower/Lab7flowerApplicationTests.java
@@ -0,0 +1,13 @@
+package ucu.edu.ua.lab7flower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab7flowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
